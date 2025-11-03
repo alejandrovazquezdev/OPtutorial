ca12912d62149c2fe2374e0e7a2e2f2c4a19a3f5
Alejandro Isaac Vazquez Lopez <alejandroisaacvazquezlopez@gmail.com>
2025-11-03 00:23:38 -0600

    Clase-16: Implementación completa de flujo de pagos Open Payments con grants interactivos y outgoing payments

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 // Configuración inicial
  import { createAuthenticatedClient, isFinalizedGrant } from "@interledger/open-payments";
  import fs from "fs";
-import { finalization } from "process";
+ import readline from "readline/promises";
 
 // a. Importar dependencias y configurar el cliente
 const privateKey = fs.readFileSync('private.key', 'utf-8')
@@ -18,8 +18,8 @@ const client = await createAuthenticatedClient({
   walletAddressUrl: 'https://ilp.interledger-test.dev/aledev',
   privateKey: 'private.Key',
   keyId: '30c571c4-3941-4221-8206-92eb2bedd81b'
-});
 
+  });
 // b. Crear una instancia del cliente Open Payments
 // c. Cargar la clave privada del archivo
 // d. Configurar las direcciones de las billeteras del remitente y el receptor
@@ -105,10 +105,112 @@ const client = await createAuthenticatedClient({
 
 // 5. Obtener una cotización para el remitente
 
+const quote = await client.quote.create(
+    {
+      url: sendingWalletAddress.resourceServer,
+      accessToken: quoteGrant.access_token.value,
+    },
+    {
+      walletAddress: sendingWalletAddress.id,
+      receiver: incomingPayment.id,
+      method: "ilp",
+    }
+  );
+
+  console.log({quote});
+
+
 // 6. Obtener una concesión para un pago saliente
+
+const outgoingPaymentGrant = await client.grant.request(
+    {
+      url: sendingWalletAddress.authServer,
+    },
+    {
+      access_token: {
+        access: [
+          {
+            type: "outgoing-payment",
+            actions: ["read", "create"],
+            limits: {
+              debitAmount: {
+                assetCode: quote.debitAmount.assetCode,
+                assetScale: quote.debitAmount.assetScale,
+                value: quote.debitAmount.value,
+              },
+            },
+            identifier: sendingWalletAddress.id,
+          },
+        ],
+      },
+      interact: {
+        start: ["redirect"],
+        // finish: {
+        //   method: "redirect",
+        //   // This is where you can (optionally) redirect a user to after going through interaction.
+        //   // Keep in mind, you will need to parse the interact_ref in the resulting interaction URL,
+        //   // and pass it into the grant continuation request.
+        //   uri: "https://example.com",
+        //   nonce: crypto.randomUUID(),
+        // },
+      },
+    }
+  );
+
+  console.log({outgoingPaymentGrant});
+
+
 
 // 7. Continuar con la concesión del pago saliente
 
+  await readline
+    .createInterface({ input: process.stdin, output: process.stdout })
+    .question("Presiona Enter para continuar con el pago saliente...");
+
+  let finalizedOutgoingPaymentGrant;
+
+
+
+
 // 8. Finalizar la concesión del pago saliente
 
+const grantContinuationErrorMessage = "\nThere was an error continuing the grant. You probably have not accepted the grant at the url (or it has already been used up, in which case, rerun the script).";
+
+  try {
+    finalizedOutgoingPaymentGrant = await client.grant.continue({
+      url: outgoingPaymentGrant.continue.uri,
+      accessToken: outgoingPaymentGrant.continue.access_token.value,
+    });
+  } catch (err) {
+    if (err instanceof OpenPaymentsClientError) {
+      console.log(grantContinuationErrorMessage);
+      process.exit();
+    }
+
+    throw err;
+  }
+
+  if (!isFinalizedGrant(finalizedOutgoingPaymentGrant)) {
+    console.log( "Se espera que finalize la concesion.");
+    process.exit();
+  }
+
+
 // 9. Continuar con la cotización de pago saliente
+const outgoingPayment = await client.outgoingPayment.create(
+    {
+      url: sendingWalletAddress.resourceServer,
+      accessToken: finalizedOutgoingPaymentGrant.access_token.value,
+    },
+    {
+      walletAddress: sendingWalletAddress.id,
+      quoteId: quote.id,
+    }
+  );
+
+  console.log(outgoingPayment);
+
+  process.exit();
+
+
+
