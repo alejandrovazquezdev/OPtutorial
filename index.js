038339712ab7bced2868d83fe67c050b7e1caaf0
Alejandro Isaac Vazquez Lopez <alejandroisaacvazquezlopez@gmail.com>
2025-11-02 21:17:13 -0600

    Clase 14: Cliente Open Payments funcional con grants y wallet addresses

--- index.js
+++ index.js
@@ -6,31 +6,68 @@
 // https://ilp.interledger-test.dev/bobdev - receptor
 
 // Configuración inicial
- import { createAuthenticatedClient } from "@interledger/open-payments";
+ import { createAuthenticatedClient, isFinalizedGrant } from "@interledger/open-payments";
  import fs from "fs";
 
 // a. Importar dependencias y configurar el cliente
-(async () => {
-    const privateKey =fs.readFileSync("./private-key.pem", "utf8");
-    const client = await createAuthenticatedClient({
-        walletAddressUrl: "https://ilp.interledger-test.dev/aledev",
-        privateKey: 'private.Key',
-        kyedId: "30c571c4-3941-4221-8206-92eb2bedd81b",
+const privateKey = fs.readFileSync('private.key', 'utf-8')
 
-    });
+// Instanciar el cliente autenticado
+const client = await createAuthenticatedClient({
+  walletAddressUrl: 'https://ilp.interledger-test.dev/aledev',
+  privateKey: 'private.Key',
+  keyId: '30c571c4-3941-4221-8206-92eb2bedd81b'
+});
 
-
-// b. Crear una instancia del cdliente Open Payments
+// b. Crear una instancia del cliente Open Payments
 // c. Cargar la clave privada del archivo
 // d. Configurar las direcciones de las billeteras del remitente y el receptor
 // Flujo de pago entre pares
+
 // 1. Obtener una concesiÃ³n para un pago entrante)
-// 2. Obtener una concesiÃ³n para un pago entrante
-// 3. Crear un pago entrante para el receptor
+  const sendingWalletAddress = await client.walletAddress.get({
+    url: "https://ilp.interledger-test.dev/aliciadev", // Make sure the wallet address starts with https:// (not $)
+  });
+  const receivingWalletAddress = await client.walletAddress.get({
+    url: "https://ilp.interledger-test.dev/bobdev", // Make sure the wallet address starts with https:// (not $)
+  });
+
+  console.log(sendingWalletAddress, receivingWalletAddress);
+
+// 2. Obtener una concesion para un pago entrante
+  const incomingPaymentGrant = await client.grant.request(
+    {
+      url: receivingWalletAddress.authServer,
+    },
+    {
+      access_token: {
+        access: [
+          {
+            type: "incoming-payment",
+            actions: ["read", "complete", "create"],
+          },
+        ],
+      },
+    }
+  );
+
+
+    if (!isFinalizedGrant(incomingPaymentGrant)) {
+        throw new Error("se espera que finalize la consesion");
+  }
+
+  console.log(incomingPaymentGrant);
+
+  // 3. Crear un pago entrante para el receptor
+
 // 4. Crear un concesiÃ³n para una cotizaciÃ³n
+
 // 5. Obtener una cotizaciÃ³n para el remitente
+
 // 6. Obtener una concesiÃ³n para un pago saliente
+
 // 7. Continuar con la concesiÃ³n del pago saliente
+
 // 8. Finalizar la concesiÃ³n del pago saliente
+
 // 9. Continuar con la cotizaciÃ³n de pago sali
-})();
